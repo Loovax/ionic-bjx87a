ac0452d372bab4c396f1026945d0667219157d7d
Loovax <bryan@loovax.com>
2019-12-08 21:10:29 -0500

    2 files modified

--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+
 import { AboutPage } from '../pages/about/about/';
-import { ContactPage } from '/pages/contact/contact';
-import { HomePage } from '/pages/home/home';
-import { ShopPage } from '/pages/shop/shop'
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { ShopPage } from '../pages/shop/shop';
 
 @Component({
   templateUrl: 'tabs.html'
