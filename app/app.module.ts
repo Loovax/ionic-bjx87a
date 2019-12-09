eafbb94ed62990c2ff2a3c768f40efb0564b5e0a
Loovax <bryan@loovax.com>
2019-12-08 21:05:41 -0500

    3 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { MyApp } from '../app/app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ShopPage } from '../pages/shop/shop';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ShopPage,
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,8 @@ import { TabsPage } from '../pages/tabs/tabs';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    ShopPage,
   ],
   providers: [
     {provide: ErrorHandler, useClass: IonicErrorHandler}
--- pages/tabs/tabs.ts
+++ pages/tabs/tabs.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AboutPage } from '../pages/about/about';
+import { AboutPage } from '../pages/about/about/';
 import { ContactPage } from '/pages/contact/contact';
 import { HomePage } from '/pages/home/home';
 import { ShopPage } from '/pages/shop/shop'
